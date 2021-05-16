af8cb16c8854d2573ff8f54768b03672eb7f4274
Chris Joakim <christopher.joakim@gmail.com>
2021-05-16 12:56:57 -0400

    imdb/row_counts.sh - SQL verification count queries

--- imdb/row_counts.sh
+++ imdb/row_counts.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 # bash script to get the row counts of the four generated csv files.
-# Chris Joakim, Microsoft, 2021/05/07
+# Chris Joakim, Microsoft, May 2021
 
+echo ''
 echo 'vertices:'
 wc -l loader_movie_vertices.csv
 wc -l loader_person_vertices.csv
 
+echo ''
 echo 'edges:'
 wc -l loader_movie_to_person_edges.csv
 wc -l loader_person_to_movie_edges.csv
+
+echo ''
+echo 'head -2 each file:'
+head -2 loader_movie_vertices.csv
+head -2 loader_person_vertices.csv
+head -2 loader_movie_to_person_edges.csv
+head -2 loader_person_to_movie_edges.csv
 
 # Output:
 # vertices:
@@ -18,8 +27,31 @@ wc -l loader_person_to_movie_edges.csv
 # edges:
 #   276552 loader_movie_to_person_edges.csv
 #   276552 loader_person_to_movie_edges.csv
+# head -2 each file:
+# Id,Pk,Label,Title,Year:int,Minutes:int
+# tt0015724,tt0015724,Movie,Dama de noche,1993,102
+# Id,Pk,Label,Name
+# nm0844752,nm0844752,Person,Rafael Sánchez Navarro
+# EdgeId,EdgePk,EdgeLabel,FromVertexId,FromVertexPk,FromVertexLabel,ToVertexId,ToVertexPk,ToVertexLabel,epoch:double
+# tt0015724-nm0844752,tt0015724-nm0844752,has_person,tt0015724,tt0015724,Movie,nm0844752,nm0844752,Person,1620420702.180296
+# EdgeId,EdgePk,EdgeLabel,FromVertexId,FromVertexPk,FromVertexLabel,ToVertexId,ToVertexPk,ToVertexLabel,epoch:double
+# nm0844752-tt0015724,nm0844752-tt0015724,is_in,nm0844752,nm0844752,Person,tt0015724,tt0015724,Movie,1620420703.6033332
 
-# (72254 + 133490)  - 2 header rows => 205742 vertices
-# (276552 + 276552) - 2 header rows => 553102 edges
+# File Math:
+#   (72254 + 133490)  - 2 header rows => 205742 vertices
+#   (276552 + 276552) - 2 header rows => 553102 edges
+#      205742 vertices + 553102 edges => 758844 total 
+
+# Verification Queries with the SQL Syntax:
+#   SELECT COUNT(1) FROM c where c.label = 'Movie'
+#   SELECT COUNT(1) FROM c where c.label = 'Person'
+#   SELECT COUNT(1) FROM c where c.label = 'has_person'
+#   SELECT COUNT(1) FROM c where c.label = 'is_in'
+#   SELECT COUNT(1) FROM c 
+# [
+#     {
+#         "$1": 758844  <-- correct number, after loading, per the above file math
+#     }
+# ]
 
 echo ''
